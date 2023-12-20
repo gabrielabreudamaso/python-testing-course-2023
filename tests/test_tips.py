6aa6a88b7c872038b218dd67465d714076c73b14
IMarty <igor.marty@telecomnancy.net>
2023-12-20 10:44:41 +0100

    exercices

--- tests/test_tips.py
+++ tests/test_tips.py
@@ -1,6 +1,7 @@
 # Function que j'ai envie d'implémenter
 def total_with_tip(bill, percentage):
-    return 120
+    total = bill + (percentage/100*bill)
+    return total
 
 # TDD - Test Driven Development
 # 1. Pour un repas à *100€* (bill), et un tips de *20%*(percentage) : Je laisse sur la table *120€*(output/return).
@@ -9,3 +10,11 @@ def test_tip_classic():
 
 def test_tip_poor_service():
     assert total_with_tip(100, 0) == 100
+
+# Exercice : écrire les tests correspondants : 
+# 2. Le tip maximal est de 500€ car il n'existe pas de billet plus gros.
+# 3. Le plus petit billet étant 5€, il n'est pas possible d'avoir un total plus bas de 5€
+# 4. Vérifer que l'arrondie du total est bien sur deux décimales
+# 5. Adater votre function d'implementation pour passer les tests
+
+# TODO:Tester les pourcentages -> Exception 
